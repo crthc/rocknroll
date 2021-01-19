af792030f3bd32b67178b30778cdd5726f2613e2
Carlos Roth <>
2021-01-19 12:36:08 +0100

    Created folders structure. Added bands and band components to pages.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BandsComponent } from './pages/bands/bands.component';
+import { BandComponent } from './pages/band/band.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BandsComponent,
+    BandComponent
   ],
   imports: [
     BrowserModule,
